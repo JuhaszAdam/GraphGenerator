3b0439295499a49c4a646d965303386dfc491c6e
Juhász Ádám <juhasz.adam@virgo.hu>
2015-05-28 16:25:31 +0200

    init2

---
+++ src/Shepard/Generator/Generator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juhasz.adam
+ * Date: 2015.05.28.
+ * Time: 16:19
+ */
+
+namespace Shepard\Generator;
+
+
+class Generator {
+
+}
---
+++ tests/Shepard/Generator/GeneratorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Shepard\Generator;
+
+
+class GeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
