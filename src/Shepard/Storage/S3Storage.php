8c7a1b31a989d2c20052b0c4a80fd34626a9c0c8
Juhász Ádám <juhasz.adam@virgo.hu>
2015-06-01 15:47:33 +0200

    S3Storage tweaking

--- src/Shepard/Storage/S3Storage.php
+++ src/Shepard/Storage/S3Storage.php
@@ -42,18 +42,6 @@ class S3Storage implements StorageInterface
      */
     public function storeContent($path, $content)
     {
-        $s3Storage = $this->client->factory();
-
-        $s3Storage->putObject([
-            'Bucket'       => $this->bucket,
-            'Key'          => $this->keyname,
-            'SourceFile'   => $path,
-            'ContentType'  => 'text/plain',
-            'ACL'          => 'public-read',
-            'StorageClass' => 'REDUCED_REDUNDANCY',
-            'Metadata'     => [
-                'content' => $content
-            ]
-        ]);
+        $this->client->upload($this->bucket, $this->keyname, $content);
     }
 }
